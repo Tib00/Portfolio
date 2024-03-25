102eb1f1556e2e90878710c6aa21b1bae9a86ad5
Asakan <thibaud.monpetit@hotmail.fr>
2024-03-25 16:34:31 +0100

    formulaire opérationnel

--- src/components/contactform/contactForm.jsx
+++ src/components/contactform/contactForm.jsx
@@ -6,12 +6,18 @@ function ContactForm() {
     <div className="contact-form-container">
        <form method='POST' netlify>
         <input type="hidden" name="form-name" value="contact" />
-          <label>Nom :</label>
-          <input type="text" id="name" name="name" required />
-          <label >Email :</label>
-          <input type="email" id="email" name="email" required />
-          <label >Message :</label>
-          <textarea id="message" name="message" required />
+          <div className="form-group">
+            <label>Nom :</label>
+            <input type="text" id="name" name="name" required />
+          </div>
+          <div className="form-group">
+            <label >Email :</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+            <div className="form-group">
+            <label >Message :</label>
+            <textarea id="message" name="message" required />
+          </div>
         <button type="submit">Envoyer</button>
       </form>
     </div>
