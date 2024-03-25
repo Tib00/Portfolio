ce10b6e39bc55cf01c1ca6ce7e17a4b20a381bc9
Asakan <thibaud.monpetit@hotmail.fr>
2024-03-25 16:49:43 +0100

    formulaire opérationnel- debugage de erreur404 après envoi partie 1

--- src/components/contactform/contactForm.jsx
+++ src/components/contactform/contactForm.jsx
@@ -4,7 +4,7 @@ import './contactForm.scss';
 function ContactForm() {
   return (
     <div className="contact-form-container">
-       <form method='POST' netlify>
+       <form id="contact" method='POST' netlify>
         <input type="hidden" name="form-name" value="contact" />
           <div className="form-group">
             <label>Nom :</label>
